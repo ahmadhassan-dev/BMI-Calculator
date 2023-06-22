9d11e9f50b98249c00b7968daaf86321400c865d
Ahmad hassan <ahmadhassan136@gmail.com>
2023-06-22 17:04:00 +0500

    Make UISLider work

--- BMI-Calculator/Controller/ViewController.swift
+++ BMI-Calculator/Controller/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var heightText: UILabel!
+    @IBOutlet weak var weightText: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func heightSlider(_ sender: UISlider) {
+        heightText.text = sender.value.description
+    }
+    
 
+    @IBAction func weightSlider(_ sender: UISlider) {
+        weightText.text = sender.value.description
+    }
 }
 
